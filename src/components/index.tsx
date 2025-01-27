ff7a04cb4b5e16f4afbae290a9fce65d836750e8
Guilherme Almeida de Oliveira <guilhermealmeida15088@gmail.com>
2025-01-27 20:34:20 -0300

    commit to debug /getShare 2

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -81,7 +81,7 @@ export function Index() {
 											: ""}
 									</p>
 									<div className="w-20 border-[1px] cursor-pointer border-neutral-400 rounded-sm py-1 pl-2 pr-5 hover:bg-black/30">
-										{value[1]}
+										{value[1].toFixed(2)}
 									</div>
 								</div>
 							))
